c9a3882e126a6f39c973b25a7c158415f4a72007
imadhou <imadhou00@gmail.com>
2021-06-05 18:15:51 +0200

    some comments

--- app.js
+++ app.js
@@ -1,24 +1,31 @@
 const express = require('express');
-//morgan for having a better log infos
+//morgan for having a better log infos it's just a dev dependency
 const morgan = require('morgan');
+
 const userRouter = require('./routes/userRouter');
 const tourRouter = require('./routes/tourRouter');
 
 const app = express();
+
+//the use function calls a middlewar and apply it to the request
+//it can be a predefined one or one that we
+//if we dont specefy the path then the middleware will be applied for all the request
+//else only for the requests on the specefied path
+//the middleware will have access to three objects req, resp and next
+//the next is a callback that should be called to pass to execute next middlewares
 
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
 //json() is a better alternative for end()
-app.use(express.json());
-app.use(express.static(`${__dirname}/public`));
-
+//app.use(express.json());
 app.use((req, resp, next) => {
   req.requestTime = new Date().toISOString();
   next();
 });
 
+//we call our routers (middleware) on the defined routes
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
